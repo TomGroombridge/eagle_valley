e35da4fbc67a572ffc88b94d5d60a4093f30e211
TomGroombridge <groombridgetom@gmail.com>
2014-06-30 20:22:44 +0100

    initial commit
    set up db and added gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EagleValley::Application.config.secret_key_base = '329239d6897c50e7110ae9377660a59d6d5da845fbbf52368638c8d58f1e0bdd4e2a60dd872f1bbc7f743c7095ae20c235601ae539508044236e09eb9a4b13cc'
